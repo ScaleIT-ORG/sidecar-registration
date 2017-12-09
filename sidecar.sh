127f427e158db5b0160e17f4f42825b18f2f5fb8
Nico Kuhn <kuhn@teco.edu>
2017-12-09 13:55:15 +0100

    Updated ETCD entries and shutdown behaviour

--- sidecar.sh
+++ sidecar.sh
@@ -3,26 +3,29 @@
 echo "Sidecar running"
 echo "pid is $$"
 
+ETCD_PORT=$ETCD_PORT
 ETCD_IP=$ETCD_IP
-APP_NAME=$APP_NAME
-APP_URL=$APP_URL
-APP_DESCRIPTION=$APP_DESCRIPTION
-APP_ICON=$APP_ICON
-echo "test: $ETCD_IP"
+echo $ETCD_PORT
+
 #check if etcd is up and running
 STR='"health": "false"'
-STR=$(curl -sb -H "Accept: application/json" "http://$ETCD_IP:49501/health")
+STR=$(curl -sb -H "Accept: application/json" "http://$ETCD_IP:$ETCD_PORT/health")
 while [[ $STR != *'"health": "true"'* ]]
 do
 	echo "Waiting for etcd ..."
-	STR=$(curl -sb -H "Accept: application/json" "http://$ETD_IP:49501/health")
+	STR=$(curl -sb -H "Accept: application/json" "http://$ETD_IP:$ETCD_PORT/health")
 	sleep 1
 done
 
 #Register Application
-curl -L -X PUT http://$ETCD_IP:49501/v2/keys/$APP_NAME/url -d value="$APP_URL"
-curl -L -X PUT http://$ETCD_IP:49501/v2/keys/$APP_NAME/icon -d value="$APP_ICON"
-curl -L -X PUT http://$ETCD_IP:49501/v2/keys/$APP_NAME/desc -d value="$APP_DESCRIPTION"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/url -d value="$APP_URL"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/App_Icon -d value="$APP_ICON"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/AppHub_Icon -d value="$APPHUB_ICON"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/description -d value="$APP_DESCRIPTION"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/lifecycleStatus -d value="Online"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/visibleForRole -d value="$APP_VISIBLEFORROLE"
+curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/appType -d value="$APP_TYPE"
+
 
 
 # SIGTERM-handler
@@ -30,7 +33,11 @@ curl -L -X PUT http://$ETCD_IP:49501/v2/keys/$APP_NAME/desc -d value="$APP_DESCR
 term_handler() {
   echo "[Sidecar] Shutting Down"
 
-  curl -L -X PUT "http://$ETCD_IP:49501/v2/keys/$APP_NAME?recursive=true" -XDELETE
+  #Delete Entry
+  #curl -L -X PUT "http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME?recursive=true" -XDELETE
+
+  #Set Status Offline
+  curl -L -X PUT http://$ETCD_IP:$ETCD_PORT/v2/keys/$APP_NAME/lifecycleStatus -d value="Offline"
 
   exit 143; # 128 + 15 -- SIGTERM
 }
